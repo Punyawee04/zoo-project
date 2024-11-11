c6d26f1a527d75efc5d0dfd851108512129fb1dd
Punyawee Sornpanya <punyawee.sor@student.mahidol.ac.th>
2024-11-11 12:53:30 +0700

    Add files via upload

--- test_zoo.py
+++ test_zoo.py
@@ -20,12 +20,27 @@ class TestZoo(unittest.TestCase):
 
     def test_elderly_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(70), 100)
-
+#  Boundary
     def test_y_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(400), 100)
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
 
     def test_X_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+
+    def test_b_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+
+    def test_a_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+
+    def test_d_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+
+    def test_c_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)   
+
+    def test_f_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
 
 if __name__ == '__main__':
     unittest.main()
